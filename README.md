a5352ad8a4ab9d1be04144a9c2909e29541c4c50
zhangwenling <2570104770@qq.com>
2023-11-29 18:02:39 +0800

    feat:新增修改提醒时间的命令，修改README文档

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-这是一个 reminder 插件，每三十分钟提醒你一次哦，记得起来活动活动
+### 这是一个 reminder 插件，每三十分钟提醒你一次哦，记得起来活动活动
+
+使用 ctrl+ Shift+ P 唤起命令列表
+
+> setUserName : 设置你的用户名
+
+> setRemindTime :设置提醒间隔时间（分钟）
--- extension.js
+++ extension.js
@@ -1,8 +1,7 @@
 const vscode = require('vscode');
-
+// 时间状态栏
 const statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
 let startTime;
-
 function updateStatusBar() {
   const currentTime = new Date().getTime();
   const timeDiff = new Date(currentTime - startTime);
@@ -17,10 +16,21 @@ function updateStatusBar() {
     displayText += `${minutes.toString().padStart(2, '0')}:`;
   }
   displayText += `${seconds.toString().padStart(2, '0')}`;
-  statusBar.text = `Time: ${displayText}`;
+  statusBar.text = `Recording Time: ${displayText}`;
   statusBar.show();
 }
-
+// 获取提醒时间
+function getReminderTime(context){
+	if(!context.globalState.get('timeInMinutes')){
+		context.globalState.update('timeInMinutes', 30);
+		return 30 * 60 * 1000
+	} else {
+		const storedTimeInMinutes = context.globalState.get('timeInMinutes');
+		console.log('已存储的时间：', storedTimeInMinutes);
+		let timer = storedTimeInMinutes * 60 * 1000; // 转换为毫秒
+		return timer;
+	}
+}
 /**
  * @param {vscode.ExtensionContext} context
  */
@@ -36,15 +46,12 @@ if (!context.globalState.get('username')) {
 } else {
 		const storedUsername = context.globalState.get('username');
 		console.log('已存储的用户名：', storedUsername);
-		let timeInMinutes = 30; // 设置你的提醒时间
-		let timer = timeInMinutes * 60 * 1000; // 转换为毫秒
-		// let timer= 3000
 		setInterval(function() {
 				vscode.window.showInformationMessage('^_^嗨！'+storedUsername+',你已经坐了半个小时啦！别盯着电脑了,看看远处吧！', '我知道了')
-		}, timer);
+		},  getReminderTime(context));
 }
-	   // 注册命令
-let disposable = vscode.commands.registerCommand('reminder.setUserName', () => {
+	   // 注册设置用户姓名命令
+let setUserName = vscode.commands.registerCommand('reminder.setUserName', () => {
 			vscode.window.showInputBox({
 					prompt: "请输入您的新用户名"
 			}).then(newUsername => {
@@ -52,22 +59,49 @@ let disposable = vscode.commands.registerCommand('reminder.setUserName', () => {
 					context.globalState.update('username', newUsername);	
 					vscode.window.showInformationMessage('用户名已更新为：' + newUsername);
 					vscode.commands.executeCommand('workbench.action.reloadWindow')
-			
 					console.log('用户名已更新为：', newUsername);
+			});
+	});
+// 注册设置提醒时间命令
+let setReminderTime = vscode.commands.registerCommand('reminder.setReminderTime', () => {
+			vscode.window.showInputBox({
+					prompt: "请输入您的新提醒时间（单位：分钟）"
+			}).then((newTimeInMinutes) => {
+				// 判断输入是否为空
+				if(!newTimeInMinutes){
+					vscode.window.showInformationMessage('提醒时间未更新');
+					return;
+				}
+				if(typeof newTimeInMinutes !== 'number' || isNaN(newTimeInMinutes)){
+					vscode.window.showInformationMessage('想什么呢！请输入数字');
+					return;
+				}
+				if(newTimeInMinutes < 1){
+					vscode.window.showInformationMessage('想什么呢！请输入大于0的数字');
+					return;
+				}
+				if(newTimeInMinutes > 60){
+					vscode.window.showInformationMessage('想什么呢！请输入小于60的数字');
+					return;
+				}
+					// 更新用户名到全局状态
+					context.globalState.update('timeInMinutes', newTimeInMinutes);	
+					vscode.window.showInformationMessage('提醒时间已更新为：' + newTimeInMinutes + '分钟');
+					vscode.commands.executeCommand('workbench.action.reloadWindow')
+					console.log('提醒时间已更新为：', newTimeInMinutes);
 
 			});
 	});
-
-	context.subscriptions.push(disposable);
-
 	startTime = new Date().getTime();
 
   setInterval(updateStatusBar, 1000);
 
   context.subscriptions.push(statusBar);
+	context.subscriptions.push(setUserName);
+	context.subscriptions.push(setReminderTime);
 }
-function deactivate() {
 
+function deactivate() {
 	statusBar.dispose();
 }
 
